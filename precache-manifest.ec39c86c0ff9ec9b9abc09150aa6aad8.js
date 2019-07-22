79d84b1bb9543235ea5e9d3c050e037b045a60fc
nabila <nabila@wowrack.com>
2019-07-22 16:26:54 +0700

    Updates

---
+++ precache-manifest.ec39c86c0ff9ec9b9abc09150aa6aad8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7c8afc7abe7492f1b81bdf88100d24",
+    "url": "/react-frontend/index.html"
+  },
+  {
+    "revision": "f96e7e25c64b6e0b515f",
+    "url": "/react-frontend/static/css/main.aae5a20b.chunk.css"
+  },
+  {
+    "revision": "cfb891cb9203f417891a",
+    "url": "/react-frontend/static/js/2.9a80981e.chunk.js"
+  },
+  {
+    "revision": "f96e7e25c64b6e0b515f",
+    "url": "/react-frontend/static/js/main.c52f2706.chunk.js"
+  },
+  {
+    "revision": "fff8fa5676271bab65f8",
+    "url": "/react-frontend/static/js/runtime~main.57f9b9d2.js"
+  },
+  {
+    "revision": "30cb12053352e272af40e98031c7102e",
+    "url": "/react-frontend/static/media/bus.30cb1205.svg"
+  },
+  {
+    "revision": "c168609eaaeae08e1dc4fde74b9ffda3",
+    "url": "/react-frontend/static/media/globe.c168609e.svg"
+  },
+  {
+    "revision": "95ac0811ebd56e53b426fd0cc28560ab",
+    "url": "/react-frontend/static/media/icon-lpr.95ac0811.svg"
+  },
+  {
+    "revision": "c2a30a4d03cbed98fe5e3b62e613010f",
+    "url": "/react-frontend/static/media/n.c2a30a4d.svg"
+  },
+  {
+    "revision": "17694fcd426f469df5d9cad1d2662d27",
+    "url": "/react-frontend/static/media/path-2.17694fcd.svg"
+  },
+  {
+    "revision": "d0cab2dc7d7acd646e7b1df6f2b053dc",
+    "url": "/react-frontend/static/media/pattern-wave-pattern-wave-copy-mask.d0cab2dc.svg"
+  },
+  {
+    "revision": "fe5e7a511aada2074a13a9078e6e6666",
+    "url": "/react-frontend/static/media/user-silhouette.fe5e7a51.svg"
+  }
+]);
